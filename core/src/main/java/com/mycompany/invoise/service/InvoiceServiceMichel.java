1702f2c26c3df39779b9c3ed89e5f1ffcc6037cd
Elo <elodie.barbe@epitech.eu>
2022-01-06 23:08:10 +0100

    New repo special Michel, preparing utilisation of database

--- core/src/main/java/com/mycompany/invoise/service/InvoiceServiceMichel.java
+++ core/src/main/java/com/mycompany/invoise/service/InvoiceServiceMichel.java
@@ -2,16 +2,17 @@ package com.mycompany.invoise.service;
 
 import com.mycompany.invoise.entity.Invoice;
 import com.mycompany.invoise.repository.InvoiceRepository;
+import com.mycompany.invoise.repository.InvoiceRepositoryMichel;
 
 public class InvoiceServiceMichel {
 
     private static long lastNumber = 0L;
 
-    private InvoiceRepository invoiceRepository = new InvoiceRepository();
+    private InvoiceRepositoryMichel invoiceRepositoryMichel = new InvoiceRepositoryMichel();
 
     public void createInvoice(Invoice invoice){
         invoice.setNumber("INV_" + ++lastNumber);
-        invoiceRepository.create(invoice);
+        invoiceRepositoryMichel.create(invoice);
     }
 }
 
