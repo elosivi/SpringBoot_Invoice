02f5e33a95a58556e7ed11efe25badb1d18fc40a
Elo <elodie.barbe@epitech.eu>
2022-01-07 16:48:28 +0100

    Injection des dependances with 4 stores options / TEST console ok for the 4 options

---
+++ core/src/main/java/com/mycompany/invoise/controller/InvoiceControllerChambouleToutMagasin2.java
@@ -0,0 +1,48 @@
+package com.mycompany.invoise.controller;
+
+
+import com.mycompany.invoise.App;
+import com.mycompany.invoise.entity.Invoice;
+import com.mycompany.invoise.service.InvoiceServiceInterface;
+
+import java.util.Scanner;
+
+public class InvoiceControllerChambouleToutMagasin2 implements InvoiceControllerInterface{
+
+    private InvoiceServiceInterface invoiceService;
+
+    public InvoiceServiceInterface getInvoiceService() {
+        return invoiceService;
+    }
+
+    public void setInvoiceService(InvoiceServiceInterface invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
+    @Override
+    public void createInvoice() {
+        System.out.println( "use of a shower scan code" );
+        String customerName = "Customer from shower";
+        Invoice invoice = new Invoice();
+        invoice.setCustomerName(customerName);
+
+        invoiceService.createInvoice(invoice);
+
+        AddOne();
+    }
+
+    public void AddOne(){
+        System.out.println(" Add one more invoice? Y/N");
+        Scanner sc = new Scanner(System.in);
+        String response = sc.nextLine();
+
+        if(response.equalsIgnoreCase("y")){
+            App.choiceInvoiceConfiguration();
+        }else if( response.equalsIgnoreCase("n")) {
+            System.out.println("Ok thank!");
+        }else{
+            System.out.println("wrong choice, please try again");
+            AddOne();
+        }
+    }
+}
