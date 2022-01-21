718751de4a73ec810a3bd5a198da9711f76dd3e6
Elo <elodie.barbe@epitech.eu>
2022-01-21 14:59:35 +0100

    Section 3.19- context app modified xml->javaConfig class

--- core/src/main/java/com/mycompany/invoise/App.java
+++ core/src/main/java/com/mycompany/invoise/App.java
@@ -1,8 +1,10 @@
 package com.mycompany.invoise;
 
 import com.mycompany.invoise.controller.InvoiceControllerInterface;
+import com.mycompany.invoise.service.InvoiceServiceInterface;
+import com.mycompany.invoise.service.prefix.InvoiceServicePrefix;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.*;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Scanner;
@@ -11,15 +13,25 @@ import java.util.Scanner;
  * Hello world!
  *
  */
+@Configuration
+@ComponentScan(basePackages = {
+        "com.mycompany.invoise.controller.web",
+        "com.mycompany.invoise.repository.database",
+        /*"com.mycompany.invoise.service.prefix"*/})
+@PropertySource("classpath:application.properties")
 public class App 
 {
     public static void main(String[] args)
     {
         //ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
         InvoiceControllerInterface invoiceControllerInterface =  context.getBean(InvoiceControllerInterface.class); //id ou polymorphisme classe de hierarchie d'héritage notemment l'interface qui implémente le contrat
         invoiceControllerInterface.createInvoice();
 
+    }
+    @Bean
+    public InvoiceServiceInterface configureInvoiceService(){
+        return new InvoiceServicePrefix();
     }
 
 }
