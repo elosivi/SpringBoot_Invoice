7912c431ba4d34dc00b7ca9b73601f05aa07ac0d
Elo <elodie.barbe@epitech.eu>
2022-01-24 09:44:42 +0100

    Section 4

--- core/src/main/java/com/mycompany/invoise/App.java
+++ core/src/main/java/com/mycompany/invoise/App.java
@@ -5,32 +5,28 @@ import com.mycompany.invoise.service.InvoiceServiceInterface;
 import com.mycompany.invoise.service.prefix.InvoiceServicePrefix;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Scanner;
 
 /**
  * Hello world!
- *
  */
-@Configuration
-@ComponentScan(basePackages = {
-        "com.mycompany.invoise.controller.web",
-        "com.mycompany.invoise.repository.database",
-        /*"com.mycompany.invoise.service.prefix"*/})
-@PropertySource("classpath:application.properties")
-public class App 
-{
-    public static void main(String[] args)
-    {
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
         //ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
-        InvoiceControllerInterface invoiceControllerInterface =  context.getBean(InvoiceControllerInterface.class); //id ou polymorphisme classe de hierarchie d'héritage notemment l'interface qui implémente le contrat
+        //ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext context = SpringApplication.run(App.class);
+        InvoiceControllerInterface invoiceControllerInterface = context.getBean(InvoiceControllerInterface.class); //id ou polymorphisme classe de hierarchie d'héritage notemment l'interface qui implémente le contrat
         invoiceControllerInterface.createInvoice();
 
     }
+
     @Bean
-    public InvoiceServiceInterface configureInvoiceService(){
+    public InvoiceServiceInterface configureInvoiceService() {
         return new InvoiceServicePrefix();
     }
 
